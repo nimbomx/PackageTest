67c58f760e0a996a1efc15f4bfea6de959bff3a5
nimbomx <ernesto@nimbo.mx>
2020-06-18 14:52:06 -0500

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
     // Nimbomx\PackageTest\src\routes\web.php
 
     Route::group(['namespace' => 'Nimbomx\PackageTest\Http\Controllers', 'middleware' => ['web']], function(){
-        Route::get('test', 'PackageTestController@index');
+        Route::get('package-test', 'PackageTestController@index');
     });
     ?>
