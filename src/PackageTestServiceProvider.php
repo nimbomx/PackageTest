1e7c933a9dc1ecdb5e709bbd274e5746a9ce3e02
nimbomx <ernesto@nimbo.mx>
2020-06-18 14:18:09 -0500

    Create PackageTestServiceProvider.php

---
+++ src/PackageTestServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+    // nimbomx\package-test\src\PackageTestServiceProvider.php
+    namespace Nimbomx\PackageTest;
+    use Illuminate\Support\ServiceProvider;
+    class PackageTestServiceProvider extends ServiceProvider {
+        public function boot()
+        {
+        }
+        public function register()
+        {
+        }
+    }
+    ?>
